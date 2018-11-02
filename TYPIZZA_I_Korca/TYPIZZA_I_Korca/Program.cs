a9dd58d34576722d89618bb37b15f41d7af473db
Kooshnir Dima <dima.koosnir@gmail.com>
2018-11-02 09:33:18 +0200

    test2

--- TYPIZZA_I_Korca/TYPIZZA_I_Korca/Program.cs
+++ TYPIZZA_I_Korca/TYPIZZA_I_Korca/Program.cs
@@ -16,7 +16,27 @@ namespace TYPIZZA_I_Korca
         {
             Bot = new TelegramBotClient("720232352:AAH_uJK6EXMelamUl-Xo-zQKUifVwLmdve8");
 
-            
+            Bot.OnMessage += BotOnMessageReceived;
+
+            var me = Bot.GetMeAsync().Result;
+
+            Console.WriteLine(me.FirstName);
+
+            Bot.StartReceiving();
+
+            Console.ReadLine();
+            Bot.StopReceiving();
+
+        }
+
+        private static void BotOnMessageReceived(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        {
+            var message = e.Message;
+
+            string name = $"{message.From.FirstName} {message.From.LastName}";
+
+
+            Console.WriteLine($"{name} отправил: '{message.Text}'");
         }
     }
 }
