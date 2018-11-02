6998ebc9ee19306b143d29c0c8e5f8364b23aee7
Kooshnir Dima <dima.koosnir@gmail.com>
2018-11-02 10:27:56 +0200

    knopki meny

--- TYPIZZA_I_Korca/TYPIZZA_I_Korca/Program.cs
+++ TYPIZZA_I_Korca/TYPIZZA_I_Korca/Program.cs
@@ -3,7 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+using Telegram.Bot.Types.InlineKeyboardButtons;
+
+
 
 namespace TYPIZZA_I_Korca
 {
@@ -17,7 +24,7 @@ namespace TYPIZZA_I_Korca
             Bot = new TelegramBotClient("720232352:AAH_uJK6EXMelamUl-Xo-zQKUifVwLmdve8");
 
             Bot.OnMessage += BotOnMessageReceived;
-
+            Bot.OnCallbackQuery += BOtOnCallbacQueryReceived;
             var me = Bot.GetMeAsync().Result;
 
             Console.WriteLine(me.FirstName);
@@ -29,14 +36,57 @@ namespace TYPIZZA_I_Korca
 
         }
 
-        private static void BotOnMessageReceived(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        private static void BOtOnCallbacQueryReceived(object sender, CallbackQueryEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static async void BotOnMessageReceived(object sender, MessageEventArgs e)
         {
             var message = e.Message;
+
+            if (message ==null ||  message.Type != MessageType.TextMessage)
+                return;
 
             string name = $"{message.From.FirstName} {message.From.LastName}";
 
 
+
             Console.WriteLine($"{name} отправил: '{message.Text}'");
+            switch (message.Text)
+            {
+                case "/start":
+                    string text =
+@"Список команд:
+/start - запуск бота
+/inline - вивiд меню
+/button - вивiд кнопочок";
+                    await Bot.SendTextMessageAsync(message.From.Id, text);
+                    break;
+
+                case "/inline":
+                    var inlineKeyboard = new InlineKeyboardMarkup(new[]
+                    {
+                        new[]
+                        {
+                            InlineKeyboardButton.WithUrl("Easy-Pizza","https://www.audit-it.ru/blogs/rabotenet/3955.php"),
+
+                            InlineKeyboardButton.WithUrl ("Hard-Pizza","https://liza.ua/lifestyle/food/10-luchshih-retseptov-pitstsyi-so-vsego-mira/")
+                               },
+                               new[]
+                               {
+                                   InlineKeyboardButton.WithCallbackData("пункт 1"),
+
+                                   InlineKeyboardButton.WithCallbackData("пункт 2")
+                               }
+
+                    });
+                    await Bot.SendTextMessageAsync(message.From.Id, "оберiть пункт меню", replyMarkup: inlineKeyboard);
+                    break;
+
+
+            }
+
+            }
         }
-    }
 }
