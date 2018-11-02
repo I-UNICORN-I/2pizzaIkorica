88c848ff359e10206b04af6dedd7da6e23f713bc
Kooshnir Dima <dima.koosnir@gmail.com>
2018-11-02 05:56:32 +0200

    z pidkluchenim tlegramom

--- TYPIZZA_I_Korca/TYPIZZA_I_Korca/Program.cs
+++ TYPIZZA_I_Korca/TYPIZZA_I_Korca/Program.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Telegram.Bot;
 
 namespace TYPIZZA_I_Korca
 {
     class Program
     {
+        static TelegramBotClient Bot;
+
+
         static void Main(string[] args)
         {
+            Bot = new TelegramBotClient("720232352:AAH_uJK6EXMelamUl-Xo-zQKUifVwLmdve8");
+
+            
         }
     }
 }
